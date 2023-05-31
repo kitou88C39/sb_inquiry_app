b2ba2b5b41aabc4500e4209a84b7c2202aad5226
kitou88C39 <kanfutrooper@icloud.com>
2023-05-31 11:30:01 +0900

    first commit

---
+++ src/test/java/Inquiry_app/InquiryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Inquiry_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
